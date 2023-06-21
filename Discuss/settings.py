5a28fda78e75e90c531991991cb3a37825bbf964
Oluwatobiloba <Oluwateezzy03@gmail.com>
2023-06-21 12:59:50 +0100

    update

--- Discuss/settings.py
+++ Discuss/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'studybud.urls'
+ROOT_URLCONF = 'Discuss.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'studybud.wsgi.application'
+WSGI_APPLICATION = 'Discuss.wsgi.application'
 
 
 # Database
