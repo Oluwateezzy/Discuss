aaac1d7b53f5a48bb6bb9bb256940f70dde21772
Oluwatobiloba <Oluwateezzy03@gmail.com>
2023-06-29 07:04:58 +0100

    update

--- base/forms.py
+++ base/forms.py
@@ -2,7 +2,9 @@ from dataclasses import fields
 from django.forms import ModelForm
 from .models import Room
 
+
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = "__all__"
+        exclude = ["host", "participants"]
