205c31dff6439b02b5ddfc58410a26cf75f727ac
Oluwatobiloba <Oluwateezzy03@gmail.com>
2023-07-17 13:11:41 +0100

    update

--- Discuss/settings.py
+++ Discuss/settings.py
@@ -17,7 +17,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = ["https://discuss-production.up.railway.app"]
 
+CORS_ALLOWED_ORIGINS = ["https://discuss-production.up.railway.app"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -28,11 +30,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "base.apps.BaseConfig",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
